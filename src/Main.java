71db6ff3f8d9a2413403c38b0dc9244421aafd67
silfo <a143050@gmail.com>
2017-09-30 14:10:13 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        double n;
+        n=in.nextDouble();
+        System.out.printf("%.1f\n",n*1.6);
+    }
+}
